152fcc8d901e609c13afd02bc929227e3b70d7f8
BapL49 <babadu4956@gmail.com>
2023-06-30 13:58:27 +0200

    analytics adding

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,32 @@
-
-
 import Header from "@/components/header";
 import "../styles/globals.css";
 import { sen } from "./font";
 import Footer from "@/components/footer";
 
+import { Analytics } from "@vercel/analytics/react";
+
 export const metadata = {
-  title: 'Brushed - modern metal poster | Discover Modern Masterpieces',
-  description: 'Explore the Brushed contemporary art marketplace and discover a curated collection of modern metals posters. Immerse yourself in the vibrant world of contemporary art. Visit us today!',
+  title: "Brushed - modern metal poster | Discover Modern Masterpieces",
+  description:
+    "Explore the Brushed contemporary art marketplace and discover a curated collection of modern metals posters. Immerse yourself in the vibrant world of contemporary art. Visit us today!",
   openGraph: {
-    title: 'Brushed - modern metal poster | Discover Modern Masterpieces',
-    description: 'Explore the Brushed contemporary art marketplace and discover a curated collection of modern metals posters. Immerse yourself in the vibrant world of contemporary art. Visit us today!',
+    title: "Brushed - modern metal poster | Discover Modern Masterpieces",
+    description:
+      "Explore the Brushed contemporary art marketplace and discover a curated collection of modern metals posters. Immerse yourself in the vibrant world of contemporary art. Visit us today!",
   },
   twitter: {
-    card: 'summary_large_image',
-    title: 'Brushed - modern metal poster | Discover Modern Masterpieces',
-    description: 'Explore the Brushed contemporary art marketplace and discover a curated collection of modern metals posters. Immerse yourself in the vibrant world of contemporary art. Visit us today!',
-
+    card: "summary_large_image",
+    title: "Brushed - modern metal poster | Discover Modern Masterpieces",
+    description:
+      "Explore the Brushed contemporary art marketplace and discover a curated collection of modern metals posters. Immerse yourself in the vibrant world of contemporary art. Visit us today!",
   },
   robots: {
     index: true,
     follow: true,
-    
-  }
-}
+  },
+};
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
       <body className={sen.variable}>
@@ -34,6 +34,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
       </body>
+      <Analytics />
     </html>
   );
 }
